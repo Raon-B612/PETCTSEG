4ad0146fabebf5faeab79c1738ffc1c88418eb5c
heartwork76 <jh.litmus@gmail.com>
2026-09-24 05:22:30 +0900

    add vertebrae_pp_refined in HC

--- CONFIG_PETCTSEG.R
+++ CONFIG_PETCTSEG.R
@@ -27,11 +27,11 @@ setkey(voi_dt, module, intensities)
 
 cols2select <- c("suv_max", "suv_mean", "hu_mean", "volume", "area")
 
-hc_modules  <- unique(voi$module) %>% .[. != "tseg_vertebrae_pp_refined"]
-modules     <- c("tseg_total", "tseg_tissue_types", "tseg_vertebrae_body", "moose_body_composition")
-# modules     <- c("tseg_total", "tseg_tissue_types", "tseg_vertebrae_body", "tseg_vertebrae_pp_refined", "moose_body_composition")
+hc_modules  <- unique(voi$module)
+modules     <- c("tseg_total", "tseg_tissue_types", "tseg_vertebrae_body", "tseg_vertebrae_pp_refined", "moose_body_composition")
 tmj_modules <- c("tseg_head_glands_cavities", "tseg_head_muscles", "tseg_headneck_bones_vessels", "tseg_headneck_muscles", "tseg_craniofacial_structures")
 ent_modules <- c(tmj_modules, modules)
+
 
 # Function to load all utility and core scripts in order [Restored]
 load_utils <- function() {
@@ -53,6 +53,3 @@ gz_all  <- c(gz_dir1, gz_dir2) |> sort()
 
 hc_dir  <- gz_all |> keep(\(x) str_detect(x, "HC[BMPVX]$"))
 tmj_dir <- gz_all |> keep(\(x) str_detect(x, "TMJ[BMPVX]$"))
-
-# doi configuration (Preserved commented-out lines)
-# doi <- c("CA", "C1", "C2", "CM", "EN", "EY", "G1", "G4", "G5", "G6", "G7", "G8", "G9", "HC", "M1", "M2", "NU", "OB", "PSMA2", "PSMA3", "PSMA", "TMJ", "TR")
